e8b78376c15957d8c6fd73d0f82456250a5d15b6
JossinAntony <jossinde@gmail.com>
2020-08-23 22:51:33 +0200

    All buttons created in homepage.dart.

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -22,11 +22,42 @@ class Homepage extends StatelessWidget {
                       borderRadius: BorderRadius.circular(8.0),
                       gradient: LinearGradient(colors: [Color.fromRGBO(170,7,107,1.0), Color.fromRGBO(97,4,95,1.0)]),
                     ),
-                    child: Center(child: Text('Largest Number')),
+                    child: Center(child: Text('Largest Number', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold))),
                   ),
                 ),
 
-                SizedBox(height: 50),
+                SizedBox(height: 30),
+
+                GestureDetector(
+                  onTap: ()=> print('container tapped'),
+                  child: Container(
+                    height: 70,
+                    width: 245,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      gradient: LinearGradient(colors: [Color.fromRGBO(221,94,137,1.0), Color.fromRGBO(247,187,151,1.0)]),
+                    ),
+                    child: Center(child: Text('Smallest Number', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold))),
+                  ),
+                ),
+
+                SizedBox(height: 30),
+
+                GestureDetector(
+                  onTap: ()=> print('container tapped'),
+                  child: Container(
+                    height: 70,
+                    width: 245,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      gradient: LinearGradient(colors: [Color.fromRGBO(229,93,135,1.0), Color.fromRGBO(95,195,228,1.0)]),
+                    ),
+                    child: Center(child: Text('Even or Odd', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold))),
+                  ),
+                ),
+
+
+
               ],
             ),
           ),

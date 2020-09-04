90321b5a1de992448446a2ab5d0b00c59405494c
JossinAntony <jossinde@gmail.com>
2020-09-04 16:43:22 +0200

    divisibility by 8- all functionality implemented.

--- lib/pages/check_divisibility.dart
+++ lib/pages/check_divisibility.dart
@@ -13,10 +13,10 @@ class _CheckDivisibilityby8State extends State<CheckDivisibilityby8> {
       setState(() {
         if((var1 % 8) == 0){
           _color = Colors.green;
-          _msg = '$var1 is divisible by 8';
+          _msg = '$var1 is divisible by 8!';
         }else{
           _color = Colors.red;
-          _msg = '$var1 is not divisible by 8';
+          _msg = '$var1 is not divisible by 8!';
         }
       });
   }
@@ -37,9 +37,35 @@ class _CheckDivisibilityby8State extends State<CheckDivisibilityby8> {
           padding: EdgeInsets.all(20.0),
           child: Column(
             children: <Widget>[
-              Text(this._msg),
+
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 70,
+                child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        style: BorderStyle.solid,
+                        width: 2,
+                        //color: Colors.blueGrey.withOpacity(.8),
+                        color: Colors.transparent,
+                      ),
+                      //color: Colors.lightBlue.withOpacity(.3),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Center(
+                      child: Text(this._msg, style: TextStyle(
+                        color: _color,
+                        fontSize: 30,
+                      )),
+                    )
+                ),
+              ),
+
+              SizedBox(height: 30),
+
               TextField(
                 controller: numCntrlr,
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
                 decoration: InputDecoration(
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(12.0),

57afc6c585c8acf432644642b442c167d7e394bb
JossinAntony <jossinde@gmail.com>
2020-09-04 16:35:42 +0200

    check even/ odd. all functionality implemented.

--- lib/pages/even_odd.dart
+++ lib/pages/even_odd.dart
@@ -10,17 +10,17 @@ class CheckEvenOdd extends StatefulWidget {
 
 class _CheckEvenOddState extends State<CheckEvenOdd> {
 
-  String _msg;
+  String _msg = '';
   Color _color;
 
   void _checkEvenOdd(var1){
     setState(() {
       if((var1 % 2) == 0){
         _color = Colors.green;
-        _msg = '$var1 is even';
+        _msg = '$var1 is even!';
       }else{
-        _color = Colors.yellow;
-        _msg = '$var1 is odd';
+        _color = Colors.red;
+        _msg = '$var1 is odd!';
       }
     });
   }
@@ -38,11 +38,35 @@ class _CheckEvenOddState extends State<CheckEvenOdd> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Text(_msg, style: TextStyle(
-                    color: _color,
-                  )),
+
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: 70,
+                    child: DecoratedBox(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            style: BorderStyle.solid,
+                            width: 2,
+                            //color: Colors.blueGrey.withOpacity(.8),
+                            color: Colors.transparent,
+                          ),
+                          //color: Colors.lightBlue.withOpacity(.3),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Center(
+                          child: Text(this._msg, style: TextStyle(
+                            color: _color,
+                            fontSize: 30,
+                          )),
+                        )
+                    ),
+                  ),
+
+                  SizedBox(height: 30),
+
                   TextField(
                     controller: numCntrlr,
+                    keyboardType: TextInputType.numberWithOptions(decimal: false),
                     decoration: InputDecoration(
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(8.0),
